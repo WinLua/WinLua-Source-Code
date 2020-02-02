d36972767f08159a87292686265bcbcaf5222b4c
russellhaley <russ.haley@gmail.com>
2020-02-02 11:37:35 -0800

    Luarocks config file and llvm-mingw.
    Added a custom action to call create-config.lua script and create the
    luarocks config file.
    
    Added llvm-mingw merge module. Works! In the process of modifying the
    above custom action to create a specialized config file for llvm-mingw.

--- WinLua-Release3/WinLua-Installer/Winlua.Installer.CustomAction/CustomAction.cs
+++ WinLua-Release3/WinLua-Installer/Winlua.Installer.CustomAction/CustomAction.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 using Microsoft.Deployment.WindowsInstaller;
 using System.Diagnostics;
+using Neo.IronLua;
 
 namespace Winlua.Installer.CustomAction
 {
@@ -14,20 +16,93 @@ namespace Winlua.Installer.CustomAction
             session.Log("Begin CustomAction1");
             if(session.Features.Contains("LuaRocks"))
             {
-                session.Log("Found LuaRocks");
-                string luarocks = session.GetTargetPath("LUAROCKS_INSTALLLOCATION");                
-                session.Log("LuaRocks installed in " + luarocks);
-                string lua_exe = session.GetTargetPath("LUA_INSTALLLOCATION");
-                session.Log("lua_exe installed in " + lua_exe);
+                Lua L = new Lua();
+                var g = L.CreateEnvironment();
+                dynamic d = g;
                 
-                RunConfigScript(session, lua_exe, luarocks);
+                session.Log("Found LuaRocks");
+                d.luaRocksRootPath = session.GetTargetPath("LUAROCKS_INSTALLLOCATION");                
+                session.Log("LuaRocks installed in " + d.luaRocksRootPath);
+                d.luaRootPath = session.GetTargetPath("LUA_INSTALLLOCATION");
+                session.Log("lua_exe installed in " + d.luaRootPath);
+
+                if (session.Features.Contains("LLVM_MingGW"))
+                {
+
+                    d.toolsPath = session.GetTargetPath("LLVMMINGW_INSTALLLOCATION");
+                    WriteConfigFile(session, g);
+                }
+                else
+                {
+                    RunConfigScript(session, d.luaRootPath, d.luaRocksRootPath);
+                }
 
             }
             return ActionResult.Success;
         }
+         
+        /// <summary>
+        /// This function manually creates the LuaRocks config file
+        /// </summary>
+        private static bool WriteConfigFile(Session session, LuaGlobal g)//string luaRootPath, string luaRocksPath, string toolsPath)
+        {
+            bool result = false;
+            ///Maybe we should re-write this as a script
+            ///so it can be inbedded into a separate tool as well?
+
+            dynamic d = g;
+            d.rocks_trees = new LuaTable();
+            d.rocks_trees[1] = new LuaTable();
+            d.rocks_trees[1].name = "user";
+            d.rocks_trees[1].root = "home..[[/luarocks]]";
+            d.rocks_trees[2] = new LuaTable();
+            d.rocks_trees[2].name = "system";
+            d.rocks_trees[2].root = d.luaRootPath;
+
+            d.variables = new LuaTable();
+            d.variables.CC = "i686-w64-mingw32-clang.exe";
+            d.variables.LD = "i686-w64-mingw32-clang.exe";
+            d.variables.LUA_LIBDIR = d.luaRootPath + "/bin";
+            d.variables.LUA_INCDIR = d.luaRootPath + "/include";
+            d.variables.LUALIB = "lua53.lib";
+
+            d.external_deps_dirs = new LuaTable();
+            d.external_deps_dirs[1] = d.toolsPath;
+
+            //I got lazy at the end...
+            g.DoChunk(@"external_deps_patterns = {
+                bin = { '?.exe', '?.bat' },
+                lib = { 'lib?.a', 'lib?.dll.a', '?.dll.a', '?.lib', 'lib?.lib'},
+                include = { '?.h' }
+                }"
+                , "c1");
+
+            ///2020-02-01: RH - NeoLua LuaTable.ToLson has a funny output: It dones't 
+            ///output the name of the table so I need to tack it on the front. Because the LuaRocks
+            ///config file contains a loose set of tables I have to:
+            ///1) prepend the table names and
+            ///2) format the individual tables in the file. 
+            string trees = "rocks_trees = " + LuaTable.ToLson(d.rocks_trees);
+            string variables = "variables = " + LuaTable.ToLson(d.variables);
+            string ex_deps_dirs = "external_deps_dirs = " + LuaTable.ToLson(d.external_deps_dirs);
+            string ex_deps_patterns = "external_deps_patterns = " + LuaTable.ToLson(d.external_deps_patterns);
+            string verbose = "verbose = false-- set to 'true' to enable verbose output";
+
+            //Ugly format string just appends each table on a new line.
+            File.WriteAllText(d.luaRocksRootPath + "/config-5.3.lua", string.Format("{0}\r\n{1}\r\n{2}\r\n{3}\r\n{4}\r\n",
+                trees, variables, ex_deps_dirs, ex_deps_patterns, verbose));
+
+            //TODO: SETX to set the LUAROCKS_SYSCONFDIR variable.
+            return result;
+
+        }
 
         private static bool RunConfigScript(Session session, string LuaPath, string LuaRocksPath)
         {
+            ///2020-02-01: RH - There is a bug in NeoLua 1.3.11 that incorrectly parses a table variable in 
+            ///the create-config.lua script (the table element is called 'const' and NeoLua handles the 5.4 syntax).
+            ///Therefore we still need to call our external lua for the time being.
+
             try
             {
                 ProcessStartInfo startInfo = new ProcessStartInfo();
