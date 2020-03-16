fd3cefc2e7589cc9aa045379223a9cdd3982b8c0
russellhaley <russ.haley@gmail.com>
2020-03-15 19:03:50 -0700

    Update to installer.
    - Added new custom actions for remove luarocks config and rocktree.
    (Doesn't work yet)
    - NOTE: Compiler is crippled to make installation tests faster.

--- WinLua-Release3/WinLua-Installer/Winlua.Installer.CustomAction/CustomAction.cs
+++ WinLua-Release3/WinLua-Installer/Winlua.Installer.CustomAction/CustomAction.cs
@@ -18,31 +18,37 @@ namespace Winlua.Installer.CustomAction
         public static ActionResult ConfigureLuaRocks(Session session)
         {
             session.Log("***WinLua: Begin CustomAction1");
-            
-                Lua L = new Lua();
-                var g = L.CreateEnvironment();
-                dynamic d = g;
-                
-                session.Log("WinLua: Found LuaRocks");
 
+            Lua L = new Lua();
+            var g = L.CreateEnvironment();
+            dynamic d = g;
+
+            d.luaRootPath = session.CustomActionData["LuaRootPath"];
+            d.luaVersion = session.CustomActionData["LuaVersion"];
+            session.Log(string.Format("WinLua: Lua version {0} is installed at {1} ", d.luaVersion, d.luaRootPath));
+
+            if (!string.IsNullOrEmpty(session.CustomActionData["LuaRocksRootPath"]))
+            {
                 d.luaRocksRootPath = session.CustomActionData["LuaRocksRootPath"];
                 session.Log("WinLua: LuaRocks installed in " + d.luaRocksRootPath);
-
-                d.luaRootPath = session.CustomActionData["LuaRootPath"];
-                session.Log("WinLua: lua_exe installed in " + d.luaRootPath);
-                
-                if (!string.IsNullOrEmpty(session.CustomActionData["ToolsPath"]))
-                {
-                    session.Log("WinLua: Installed LLVM (aparently)");
-                    d.toolsPath = session.CustomActionData["ToolsPath"];
-                    WriteConfigFile(session, g);
-                    session.Log("WinLua: wrote config file for llvm");
-                }
-                else
-                {
-                    session.Log("WinLua: didn't find llvm use luarocks");
-                    RunConfigScript(session, d.luaRootPath, d.luaRocksRootPath);
-                }
+            }
+            else
+            {
+                session.Log("WinLua: LuaRocks not installed.");
+            }
+                            
+            if (!string.IsNullOrEmpty(session.CustomActionData["ToolsPath"]))
+            {
+                session.Log("WinLua: WinLua Compiler Tools (WLC) have been installed.");
+                d.toolsPath = session.CustomActionData["ToolsPath"];
+                WriteConfigFile(session, g);
+                session.Log("WinLua: Wrote config file for use with WLC");
+            }
+            else
+            {
+                session.Log("WinLua: WinLua Compiler Tools (WLC) were not installed");
+                RunConfigScript(session, d.luaRootPath, d.luaRocksRootPath);
+            }
             return ActionResult.Success;
         }
          
@@ -58,38 +64,39 @@ namespace Winlua.Installer.CustomAction
             ///This is a first crack at creating the config file, 
             ///it's also a test of what I can do with NeoLua so 
             ///bear with me...
-            session.Log("WinLua: Begin Write");
+            session.Log("WinLua: Create Config File for LuaRocks with WLC.");
             dynamic d = g;
+            //This is creating tables and table values in C# using dynamic
             d.rocks_trees = new LuaTable();
             d.rocks_trees[1] = new LuaTable();
             d.rocks_trees[1].name = "user";
             d.rocks_trees[1].root = "home..[[/luarocks]]";
             d.rocks_trees[2] = new LuaTable();
             d.rocks_trees[2].name = "system";
-            g.DoChunk(@"luaRootPath = luaRootPath:gsub('\\','/')", "c1");
-            //TODO: Hard coded 5.3
-            d.luaRootPath += "5.3/";
+            g.DoChunk(@"luaRootPath = luaRootPath:gsub('\\','/').. luaVersion .. '/'", "c1");            
             d.rocks_trees[2].root = d.luaRootPath;
 
-            d.variables = new LuaTable();
-            
-            d.variables.LUA_LIBDIR = d.luaRootPath + "bin";
-            d.variables.LUA_INCDIR = d.luaRootPath + "include";
-            
-            //TODO: Remove hard code lib filename
-            d.variables.LUALIB = "lua53.lib";
+            //This is creating tables and values in Lua
+            ///- Create the variables table and add LUA_LIBDIR and LUA_INCDIR.
+            ///- Format the lua version and create the path for the new config file
+            ///- Create the lua lib filename
+            ///- Create the LuRocks config file path and name
+            session.Log("WinLua: Running custom Lua chunk...");
             g.DoChunk(@"
-                for i,v in pairs(variables) do
-                    if type(v) == 'string' then
-                        variables[i] = v:gsub('\\','/')
-                    end
-                end", "c1");
+            variables = {}
+            lua_version_formatted = luaVersion:gsub('%.','')                        
+            variables.LUA_LIBDIR = luaRootPath..'bin/'
+            variables.LUA_INCDIR = luaRootPath..'include/'            
+            variables.LUALIB = string.format('lua%s.lib', lua_version_formatted)
+            luaRocksConfigPath = string.format('%sconfig-%s.lua', luaRocksRootPath, luaVersion)
+            ", "c1");
 
             if (d.toolsPath != null)
             {
+                session.Log("WinLua: Found tools path.");
+                ///- Names where the mingw library sit
                 d.x86_lib = "i686-w64-mingw32";
                 d.x64_lib = "x86_64-w64-mingw32";
-                session.Log("WinLua: Found Tools path");
                 //TODO: Hard coded Processor names
                 d.variables.CC = "wlc32.exe";
                 d.variables.LD = "wlc32.exe";
@@ -97,7 +104,7 @@ namespace Winlua.Installer.CustomAction
                 d.external_deps_dirs = new LuaTable();
                 
                 //TODO: Remove hard code tools lib path (needs to be arch independant)
-                d.external_deps_dirs[1] = d.toolsPath + "\\" +  d.x86_lib;
+                d.external_deps_dirs[1] = d.toolsPath + d.x86_lib + "/";
                 g.DoChunk(@"external_deps_dirs[1] = external_deps_dirs[1]:gsub('\\','/')", "c1");
                 //I got lazy at the end...
                 g.DoChunk(@"
@@ -107,32 +114,36 @@ namespace Winlua.Installer.CustomAction
                         include = { '?.h' }
                     }", "c1");
             }
+            else
+            {
+                session.Log("WinLua: Tools path not found.");
+            }
 
             //Prepend the table names for formatting in the config file.
-
             string trees = "rocks_trees = " + LuaTable.ToLson(d.rocks_trees);
             string variables = "variables = " + LuaTable.ToLson(d.variables);
             string ex_deps_dirs = "";
             string ex_deps_patterns = "";
+            string verbose = "verbose = false-- set to 'true' to enable verbose output";
 
             if (d.toolsPath != null)
             {
-                session.Log("WinLua: Format external_*");
+                session.Log("WinLua: Format external_* tables");
                 ex_deps_dirs = "external_deps_dirs = " + LuaTable.ToLson(d.external_deps_dirs);
                 ex_deps_patterns = "external_deps_patterns = " + LuaTable.ToLson(d.external_deps_patterns);
                 
             }
-            string verbose = "verbose = false-- set to 'true' to enable verbose output";
-
-            //TODO: Remove the hard coded config file name!
+                        
             //Write to file, append each table to the file (newline seperated). 
-            File.WriteAllText(d.luaRocksRootPath + "\\config-5.3.lua", string.Format("{0}\r\n{1}\r\n{2}\r\n{3}\r\n{4}\r\n",
+            //Note, the d.luaRocksConfigPath variable is created in a Lua chunk above.
+            File.WriteAllText(d.luaRocksConfigPath, string.Format("{0}\r\n{1}\r\n{2}\r\n{3}\r\n{4}\r\n",
                 trees, variables, ex_deps_dirs, ex_deps_patterns, verbose));
 
-            session.Log("Done!");
+            session.Log(string.Format("WinLua: Created config file at {0}.", d.luaRocksRootPath));
             //TODO: Do this properly in the installer, not in the custom action. It's too opaque.
             //Set the LUAROCKS_SYSCONFDIR variable.
-            Environment.SetEnvironmentVariable(LUAROCKS_SYSCONFDIR, d.luaRocksRootPath, EnvironmentVariableTarget.Machine);            
+            Environment.SetEnvironmentVariable(LUAROCKS_SYSCONFDIR, d.luaRocksRootPath, EnvironmentVariableTarget.Machine);
+            session.Log("WinLua: Set LUAROCKS_SYSCONFDIR. LuaRocks configuration complete.");
             return result;
 
         }
@@ -171,11 +182,90 @@ namespace Winlua.Installer.CustomAction
         public static ActionResult RemoveLuaRocksConfig(Session session)
         {            
             session.Log("***WinLua: Begin Removing Config File");
-            var db = session.Database;
-            Guid id = Guid.NewGuid();
 
+            string luaRootPath = session.CustomActionData["LuaRootPath"];
+            string luaVersion = session.CustomActionData["LuaVersion"];
+            session.Log(string.Format("WinLua: Lua version {0} is installed at {1} ", luaVersion, luaRootPath));
+            string luaRocksRootPath = "";
+            string toolsPath = "";
+            string luaRocksConfigFile = "";
+            //TODO: Test for user option to leave Config file in place
+            if (!string.IsNullOrEmpty(session.CustomActionData["LuaRocksRootPath"]))
+            {
+                luaRocksRootPath = session.CustomActionData["LuaRocksRootPath"];
+                session.Log("WinLua: LuaRocks installed in " + luaRocksRootPath);
+                luaRocksConfigFile = string.Format("{0}config-{1}.lua", luaRocksRootPath, luaVersion.Replace(".", ""));
+            }
+            else
+            {
+                session.Log("WinLua: LuaRocks not installed.");
+            }
+
+            if (!string.IsNullOrEmpty(session.CustomActionData["ToolsPath"]))
+            {
+                session.Log("WinLua: WinLua Compiler Tools (WLC) have been installed.");
+                toolsPath = session.CustomActionData["ToolsPath"];
+                session.Log("WinLua: Wrote config file for use with WLC");
+            }
+            else
+            {
+                session.Log("WinLua: WinLua Compiler Tools (WLC) were not installed");
+            }
+
+            //build luarocks config file name/path
+
+            if (File.Exists(luaRocksConfigFile))
+            {
+                session.Log("WinLua: Deleting LuaRocks Config file.");
+                File.Delete(luaRocksConfigFile);
+            }
             Environment.SetEnvironmentVariable(LUAROCKS_SYSCONFDIR, null, EnvironmentVariableTarget.Machine);
             session.Log("***WinLua: Config File Removed");
+            return ActionResult.Success;
+        }
+
+        [CustomAction]
+        public static ActionResult RemoveRockTree(Session session)
+        {
+            session.Log("***WinLua: Begin Removing RockTree");
+
+            string luaRootPath = session.CustomActionData["LuaRootPath"];
+            string luaVersion = session.CustomActionData["LuaVersion"];
+            session.Log(string.Format("WinLua: Lua version {0} is installed at {1} ", luaVersion, luaRootPath));
+            string luaRocksRootPath = "";
+            string toolsPath = "";
+            string rockTree = "";
+            //TODO: Test for user option to leave Config file in place
+            if (!string.IsNullOrEmpty(session.CustomActionData["LuaRocksRootPath"]))
+            {
+                luaRocksRootPath = session.CustomActionData["LuaRocksRootPath"];
+                session.Log("WinLua: LuaRocks installed in " + luaRocksRootPath);
+                rockTree = string.Format("{0}NOT A REAL ROCK TREE{1}.lua", luaRocksRootPath, luaVersion.Replace(".", ""));
+            }
+            else
+            {
+                session.Log("WinLua: LuaRocks not installed.");
+            }
+
+            if (!string.IsNullOrEmpty(session.CustomActionData["ToolsPath"]))
+            {
+                session.Log("WinLua: WinLua Compiler Tools (WLC) have been installed.");
+                toolsPath = session.CustomActionData["ToolsPath"];
+                session.Log("WinLua: Wrote config file for use with WLC");
+            }
+            else
+            {
+                session.Log("WinLua: WinLua Compiler Tools (WLC) were not installed");
+            }
+
+            //build luarocks config file name/path
+
+            if (Directory.Exists(rockTree))
+            {
+                session.Log("WinLua: Removing Rocktree.");
+                Directory.Delete(rockTree, true);
+            }
+            session.Log("***WinLua: Rocktree Removed");
             return ActionResult.Success;
         }
     }
